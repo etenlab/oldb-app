ec414a5164bfa478b150165b401ad7e38c6851f8
Vivek Varma <vivekvarma15516@gmail.com>
2022-11-11 04:15:49 +0530

    show basic table on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,58 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Home from "./pages/Home";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://fast-heron-34.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        <Route path='/SLI' component={() => {
-          window.location.href = 'https://cartographa.netlify.app/#close';
-          return null;
-        }} />
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <ApolloProvider client={client}>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route
+            path="/SLI"
+            component={() => {
+              window.location.href = "https://cartographa.netlify.app/#close";
+              return null;
+            }}
+          />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </ApolloProvider>
   </IonApp>
 );
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,82 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import { AgGridReact } from "ag-grid-react";
+import { gql, useQuery } from "@apollo/client";
 
 const Home: React.FC = () => {
+  const [rowData, setRowData] = useState([]);
+  const GET_ROW_DATA = gql`
+    query MyQuery {
+      progress_bible_language_details(limit: 10, offset: 0) {
+        code_status
+        ethnologue_name
+        how_to_fix
+        id
+        is_sign_language
+        iso_639_3_code
+        language_scope
+        language_status
+        primary_continent
+        primary_country_code
+        primary_country_name
+        retired_date
+        show_active_dialect
+        retirement_explanation
+        show_active_language
+        show_retired_dialect
+        show_retired_language
+        show_sign_language
+        unit_code
+        unit_full_name
+        unit_name
+        unit_type
+      }
+    }
+  `;
+
+  const columnDefs = [
+    { field: "id" },
+    { field: "unit_code" },
+    { field: "unit_type" },
+    { field: "unit_name" },
+    { field: "unit_full_name" },
+    { field: "ethnologue_name" },
+    { field: "iso_639_3_code" },
+    { field: "is_sign_language" },
+    { field: "code_status" },
+    { field: "language_status" },
+    { field: "language_scope" },
+    { field: "primary_continent" },
+    { field: "primary_country_name" },
+    { field: "primary_country_code" },
+    { field: "retirement_explanation" },
+    { field: "how_to_fix" },
+    { field: "retired_date" },
+    { field: "show_active_language" },
+    { field: "show_retired_language" },
+    { field: "show_active_dialect" },
+    { field: "show_retired_dialect" },
+    { field: "show_sign_language" },
+  ];
+  const { loading, error, data } = useQuery(GET_ROW_DATA, {
+    variables: { language: "english" },
+  });
+  useEffect(() => {}, []);
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+  // if (error) {
+  //   return <div>Error! {error.message}</div>;
+  // } else
+  console.log("graphqlData", data);
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +90,15 @@ const Home: React.FC = () => {
             <IonTitle size="large"></IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <div
+          className="ag-theme-alpine"
+          style={{ width: "auto", height: "90vh" }}
+        >
+          <AgGridReact
+            rowData={data ? data.progress_bible_language_details : []}
+            columnDefs={columnDefs}
+          ></AgGridReact>
+        </div>
       </IonContent>
     </IonPage>
   );
